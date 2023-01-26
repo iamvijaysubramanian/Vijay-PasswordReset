3dbd528f549eed158f5f2da7b49c9716a11126ef
iamvijaysubramanian <vijaysubbu69@gmail.com>
2023-01-26 14:20:34 +0530

    deploy link changed

--- src/components/ForgotPassword/index.jsx
+++ src/components/ForgotPassword/index.jsx
@@ -10,7 +10,7 @@ const ForgotPassword = () => {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		try {
-			const url = `http://localhost:3001/api/password-reset`;
+			const url = `https://password-reset-xejh.onrender.com/password-reset`;
 			const { data } = await axios.post(url, { email });
 			setMsg(data.message);
 			setError("");
--- src/components/Signup/index.jsx
+++ src/components/Signup/index.jsx
@@ -20,7 +20,7 @@ const Signup = () => {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		try {
-			const url = "http://localhost:3001/api/users";
+			const url = "https://password-reset-xejh.onrender.com/users";
 			const { data: res } = await axios.post(url, data);
 			navigate("/login");
 			console.log(res.message);
